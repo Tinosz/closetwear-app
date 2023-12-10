90957cdc9feed6d021067193d055460b5a814a4d
Stevenleez <stevenaxel2003@gmail.com>
2023-12-11 05:06:21 +0700

    yyh

--- frontend/src/pages/administrator/EditCategory.jsx
+++ frontend/src/pages/administrator/EditCategory.jsx
@@ -5,6 +5,8 @@ import { useStateContext } from "../../context/ContextProvider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
+import "./styles/EditCategoryStyles.css";
+
 export default function EditCategory() {
     const { id } = useParams();
     const navigate = useNavigate();
@@ -157,36 +159,47 @@ export default function EditCategory() {
 
     return (
         <>
-            {notification && <div>{notification}</div>}
-            <form onSubmit={onSubmit}>
-                <p>{isLoading ? "Loading..." : (id ? `Update: ${category.category_name}` : "")}</p>
-                {errors && errors.category_name && (
-                    <p className="text-red-600">{errors.category_name}</p>
-                )}
-                <input
-                    placeholder="Category Name"
-                    onChange={(e) =>
-                        setCategory({
-                            ...category,
-                            category_name: e.target.value,
-                        })
-                    }
-                    value={category.category_name}
-                />
-                <label>
+        <div className="container">
+            {notification && 
+            <div className="notification">{notification}</div>}
+            <form className="max-w-sm mx-auto" onSubmit={onSubmit}>
+                <div className="mb-5">
+                    <p className="loading-text">{isLoading ? "Loading..." : (id ? `Update: ${category.category_name}` : "")}</p>
+
+                    
+                    {errors && errors.category_name && (
+                        <p className="text-red-600">{errors.category_name}</p>
+                    )}
                     <input
-                        type="checkbox"
-                        checked={Number(category.featured) === 1}
-                        onChange={() => setCategory({
-                            ...category,
-                            featured: category.featured === 1 ? 0 : 1
-                        })}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Category Name"
+                        onChange={(e) =>
+                            setCategory({
+                                ...category,
+                                category_name: e.target.value,
+                            })
+                        }
+                        value={category.category_name}
                     />
-                    Featured?
-                </label>
-                <button type="submit">
-                    {id ? "Update Category" : "Add Category"}
-                </button>
+                    
+                    <div className="flex items-start mb-5">
+                        <div className="flex items-center h-5">
+                            <input
+                                type="checkbox"
+                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                                checked={Number(category.featured) === 1}
+                                onChange={() => setCategory({
+                                    ...category,
+                                    featured: category.featured === 1 ? 0 : 1
+                                })}
+                            />
+                        </div>
+                        <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+                    </div>
+                    <div className="action-button" onClick={onSubmit}>
+                        {id ? "Update Category" : "Add Category"}
+                    </div>
+                </div>
             </form>
             <div>
                 <label>
@@ -250,6 +263,7 @@ export default function EditCategory() {
                     Delete Selected
                 </button>
             </div>
+        </div>
         </>
     );
 }
