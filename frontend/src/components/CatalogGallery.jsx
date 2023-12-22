8bd953fb34a9df1a1f9f4ba5a94026125e3ed126
marthinrl <85492642+mrthnrldo@users.noreply.github.com>
2023-12-22 12:01:38 +0700

    buat catalog responsive

--- frontend/src/components/CatalogGallery.jsx
+++ frontend/src/components/CatalogGallery.jsx
@@ -133,7 +133,7 @@ export default function CatalogGallery() {
     };
 
     const ImageGallery = () => (
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div className="p-1 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
             {searchWord === ""
                 ? items
                       .filter((item) => item.available_stock === "1") // Filter items with available_stock === '1'
